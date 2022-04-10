34945feba9b5b0433105b3f7c6db0b328f3eb9ec
1190200501 <linyyan.lin@gmail.com>
2022-04-10 11:03:01 +0800

    finish

--- Lab2/Code/semantics.c
+++ Lab2/Code/semantics.c
@@ -1,7 +1,8 @@
 #include "semantics.h"
 
 
-Var vartable[SIZE+1];//储存结构体或变量
+Vari varitable[SIZE+1];//储存结构体或变量
+Function functiontable[SIZE + 1];
 int struct_without_name=0;
 
 
@@ -16,9 +17,9 @@ unsigned int hash_pjw(char* name){
     return val;
 }
 // check if a name is in the table
-Var find_var_table(char* name){
+Vari find_vari_table(char* name){
     unsigned int val = hash_pjw(name);
-    Var x = vartable[val];
+    Vari x = varitable[val];
     while (x != NULL) {
         if (strcmp(x->field->name, name) == 0)
             break;
@@ -26,26 +27,26 @@ Var find_var_table(char* name){
     }
     return x;
 }
-Var create_var(int in_struct, FieldList field, Var next, int line) {
-    Var var_table = (Var)malloc(sizeof(struct Var_));
-    var_table->is_def_struct = in_struct;
-    var_table->field = field;
-    var_table->next = next;
-    var_table->line = line;
-    return var_table;
+Vari create_vari(int in_struct, FieldList field, Vari next, int line) {
+    Vari vari_table = (Vari)malloc(sizeof(struct Vari_));
+    vari_table->is_def_struct = in_struct;
+    vari_table->field = field;
+    vari_table->next = next;
+    vari_table->line = line;
+    return vari_table;
 }
 // add a field to the table
-void insert_var_table(FieldList field_list, int line, int in_struct){
+void insert_vari_table(FieldList field_list, int line, int in_struct){
     int val = hash_pjw(field_list->name);
-    if (vartable[val] == NULL) {
-        vartable[val] = create_var(in_struct, field_list, NULL, line);
+    if (varitable[val] == NULL) {
+        varitable[val] = create_vari(in_struct, field_list, NULL, line);
         return;
     }
-    Var now = vartable[val];
+    Vari now = varitable[val];
     while (now->next != NULL) {
         now = now->next;
     }
-    now->next = create_var(in_struct, field_list, NULL, line);
+    now->next = create_vari(in_struct, field_list, NULL, line);
 }
 
 // compare two types
@@ -76,14 +77,48 @@ int fieldcmp(FieldList a,FieldList b, int compare_name) {
     return fieldcmp(a->next, b->next, compare_name);
 }
 
+Function find_function_table(char* name) {
+    unsigned int val = hash_pjw(name);
+    Function x = functiontable[val];
+    while (x != NULL) {
+        if (strcmp(x->name, name) == 0)
+            break;
+        x = x->next;
+    }
+    return x;
+}
+
+void insert_function_table(Function func) {
+    unsigned int val = hash_pjw(func->name);
+    if (functiontable[val] == NULL) {
+        functiontable[val] = func;
+        return;
+    }
+    Function now = functiontable[val];
+    while (now->next != NULL) {
+        now = now->next;
+    }
+    now->next = func;
+}
+
+int funccmp(Function a, Function b) {
+    if (a == NULL && b == NULL)
+        return 0;
+    if (a == NULL || b == NULL)
+        return 1;
+    if (strcmp(a->name, b->name))
+        return 1;
+    if (fieldcmp(a->field, b->field, 1))
+        return 1;
+    if (typecmp(a->return_type, b->return_type))
+        return 1;
+    return 0;
+}
 // 建立变量表 查找错误
 void semantic_analysis(P_Node now){
-    for(int i=0;i<SIZE;i++){
-        vartable[i]=NULL;
-    }
     // Program -> ExtDefList
     P_Node extdeflist=now->firstchild;
-    while(extdeflist->firstchild!=NULL){
+    while(extdeflist->firstchild!=NULL && extdeflist->firstchild->Token!=NULL){
         // ExtDefList -> ExtDef ExtDefList| empty
         P_Node extdef=extdeflist->firstchild;
         ExtDef(extdef);
@@ -98,7 +133,6 @@ void ExtDef(P_Node now){
     */
    P_Node specifier=now->firstchild;
    Type specifier_type=Specifier(specifier);
-   //在使用type时，要注意type==null的情况
    P_Node fir_bro=specifier->nextbro;
    P_Node sec_bro=fir_bro->nextbro;
    if(sec_bro==NULL){
@@ -111,6 +145,11 @@ void ExtDef(P_Node now){
         //根据specifier设定extdeclist变量类型
         ExtDecList(fir_bro,specifier_type);
    }
+   else if(!strcmp(fir_bro->Token,"FunDec")){
+        //ExtDef → Specifier ExtDecList SEMI
+        FunDec(fir_bro,specifier_type,DEF_FUNC);
+        CompSt(fir_bro->nextbro,specifier_type);
+   }
 }
 
 Type Specifier(P_Node now){
@@ -121,10 +160,10 @@ Type Specifier(P_Node now){
         Type return_type=(Type)malloc(sizeof(struct Type_));
         return_type->type=BASIC;
         if(!strcmp(child->Id_Type,"int")){
-            return_type->basic=TYPE_INT;
+            return_type->basic=T_INT;
         }
         else{
-            return_type->basic=TYPE_FLOAT;
+            return_type->basic=T_FLOAT;
         }
         return return_type;
     }
@@ -141,6 +180,7 @@ Type StructSpecifier(P_Node now){
     P_Node defList=optTag->nextbro->nextbro;
     
     if (!strcmp(optTag->Token,"OptTag")){
+        //StructSpecifier → STRUCT OptTag LC DefList RC
         //OptTag → ID| empty
         FieldList struct_field=(FieldList)malloc(sizeof(struct FieldList_));
         Type type=(Type)malloc(sizeof(struct Type_));
@@ -151,13 +191,13 @@ Type StructSpecifier(P_Node now){
         struct_field->next = NULL;
         if (struct_field->name == NULL)
             return NULL;
-        insert_var_table(struct_field, now->firstchild->line, DEF_IN_STRUCT);
+        insert_vari_table(struct_field, now->firstchild->line, DEF_IN_STRUCT);
         type->structure = struct_field;
         return type;
     }else {
         // StructSpecifier -> STRUCT Tag
         // Tag -> ID
-        Var new = find_var_table(struct_token->nextbro->firstchild->Id_Type);
+        Vari new = find_vari_table(struct_token->nextbro->firstchild->Id_Type);
         return new->field->type;
     }
 }
@@ -179,7 +219,7 @@ FieldList DefList(P_Node now,int judge){
     P_Node newdef=now->firstchild;
     FieldList head=NULL;
     FieldList nownode=NULL;
-    while(newdef!=NULL){
+    while(newdef!=NULL && newdef->Token!=NULL){
         FieldList nxt=Def(newdef,judge);
         if(head==NULL) {
             head=nownode=nxt;
@@ -221,16 +261,16 @@ FieldList DecList(P_Node now,Type type,int judge){
 
 FieldList Dec(P_Node now,Type type,int judge){
     //Dec → VarDec| VarDec ASSIGNOP Exp
-    P_Node vardec=now->firstchild;
-    P_Node fir_bro=vardec->nextbro;
+    P_Node varidec=now->firstchild;
+    P_Node fir_bro=varidec->nextbro;
     if(fir_bro==NULL){
         //Dec → VarDec
-        return VarDec(vardec,type,judge);
+        return VarDec(varidec,type,judge);
     }
     else{
         //Dec → VarDec ASSIGNOP Exp
         if(judge==DEF_STRUCT){
-            printf("Error type 15 at Line %d: Initialize domain in structure.\n",vardec->line);
+            printf("Error type 15 at Line %d: Initialize domain in structure.\n",varidec->line);
             return NULL;
         }
         Type exp_type=Exp(fir_bro->nextbro);
@@ -239,7 +279,7 @@ FieldList Dec(P_Node now,Type type,int judge){
             return NULL;
             
         }
-        return VarDec(vardec,type,judge);
+        return VarDec(varidec,type,judge);
     }
 }
 
@@ -254,7 +294,7 @@ FieldList VarDec(P_Node now,Type type,int judge){
         fieldlist->type=type;
         fieldlist->next=NULL;
 
-        if (find_var_table(fieldlist->name)){
+        if (find_vari_table(fieldlist->name)){
             if (judge == DEF_STRUCT){
                 printf("Error type 15 at Line %d: Redefined structure's domain \"%s\".\n",now->line,child->Id_Type);
                 return NULL;
@@ -264,7 +304,7 @@ FieldList VarDec(P_Node now,Type type,int judge){
                 return NULL;
             }
         }
-        insert_var_table(fieldlist,child->line,DEF_NOT_IN_STRUCT);
+        insert_vari_table(fieldlist,child->line,DEF_NOT_IN_STRUCT);
         return fieldlist;
     }
     else{
@@ -280,14 +320,49 @@ FieldList VarDec(P_Node now,Type type,int judge){
 
 void ExtDecList(P_Node now,Type type){
     //ExtDecList → VarDec| VarDec COMMA ExtDecList
-    P_Node vardec=now->firstchild;
-    P_Node fir_bro=vardec->nextbro;
-    VarDec(vardec,type,DEF_VAR);
+    P_Node varidec=now->firstchild;
+    P_Node fir_bro=varidec->nextbro;
+    VarDec(varidec,type,DEF_VAR);
     if(!(fir_bro==NULL)){
         //ExtDecList → VarDec COMMA ExtDecList
         ExtDecList(fir_bro->nextbro,type);
     }
 }
+
+void FunDec(P_Node now, Type type, int judge) {
+    // FunDec -> ID LP VarList RP | ID LP RP
+    P_Node id = now->firstchild;
+    P_Node varlist = id->nextbro->nextbro;
+    Function func = (Function)malloc(sizeof(struct Function_));
+    func->name = id->Id_Type;
+    func->field = NULL;
+    func->return_type = type;
+    func->defined = 0;
+    func->line = now->line;
+    func->next = NULL;
+    if (varlist->nextbro) {
+        func->field = VarList(varlist, judge);
+    }
+    // printf("@@@@@@@@\n");
+    Function find_func = find_function_table(func->name);
+    if (find_func == NULL) {
+        func->defined = 1;
+        insert_function_table(func);
+    } else {
+        if (funccmp(find_func, func) == 0) {
+            if (func->defined) {
+                printf("Error type 4 at Line %d: Redefined function \"%s\".\n", now->line, func->name);
+                return;
+            } else
+                find_func->defined = 1;
+        } else {
+            printf("Error type 4 at Line %d: Redefined function \"%s\".\n", now->line, func->name);
+            return;
+        }
+    }
+    
+}
+
 FieldList VarList(P_Node now,int judge){
     //VarList → ParamDec COMMA VarList| ParamDec
     P_Node paramdec=now->firstchild;
@@ -311,6 +386,73 @@ FieldList ParamDec(P_Node now,int judge){
     return VarDec(now->firstchild->nextbro,specifier_type,judge);
 }
 
+void CompSt(P_Node now,Type type){
+    //CompSt → LC DefList StmtList RC
+    P_Node deflist=now->firstchild->nextbro;
+    P_Node stmtlist=deflist->nextbro;
+
+    DefList(deflist,DEF_VAR);
+    StmtList(stmtlist,type);
+}
+
+void StmtList(P_Node now,Type type){
+    //StmtList → Stmt StmtList| empty
+    if(now->Token==NULL)
+        return;
+    P_Node child=now->firstchild;
+    // printf("(\n");
+    Stmt(child,type);
+    // printf(")\n");
+    StmtList(child->nextbro,type);
+}
+
+void Stmt(P_Node now,Type type){
+    // printf("%d %d\n", __LINE__, now);
+    P_Node child=now->firstchild;
+    if(strcmp(child->Token,"Exp")==0){
+        //Stmt → Exp SEMI
+        Exp(child);
+    }
+    else if(strcmp(child->Token,"CompSt")==0){
+        //Stmt →CompSt
+        CompSt(child,type);
+    }
+    else if(strcmp(child->Token,"RETURN")==0){
+        //Stmt →RETURN Exp SEMI
+        Type exp_type=Exp(child->nextbro);
+        if(typecmp(exp_type,type)==1){
+            printf("Error type 8 at Line %d: Type mismatched for return.\n",now->line);
+            return;
+        }
+    }
+    else if(strcmp(child->Token,"IF")==0){
+        //Stmt →IF LP Exp RP Stmt|IF LP Exp RP Stmt ELSE Stmt
+        P_Node exp_in_if=child->nextbro->nextbro;
+        Type exp_type=Exp(exp_in_if);
+        if(exp_type==NULL) return;
+        if(exp_type->type!=BASIC||exp_type->basic!=T_INT){
+            printf("Error type 7 at Line %d: Type mismatched for operands.\n",exp_in_if->line);
+            return;
+        }
+        P_Node stmt = exp_in_if->nextbro->nextbro;
+        Stmt(stmt,type);
+        if(stmt->nextbro){
+            Stmt(stmt->nextbro->nextbro,type);
+        }
+    }
+    else if(strcmp(child->Token,"WHILE")==0){
+        //Stmt →WHILE LP Exp RP Stmt
+        P_Node exp_in_while=child->nextbro->nextbro;
+        Type exp_type=Exp(exp_in_while);
+        if(exp_type==NULL) return;
+        if(exp_type->type!=BASIC||exp_type->basic!=T_INT){
+            printf("Error type 7 at Line %d: Type mismatched for operands.\n",exp_in_while->line);
+            return;
+        }
+        Stmt(exp_in_while->nextbro->nextbro,type);
+    }   
+}
+
 Type Exp(P_Node now){
     //printf("aExp\n");
     P_Node child=now->firstchild;
@@ -321,20 +463,20 @@ Type Exp(P_Node now){
         return_type->type=BASIC;
         if(strcmp(child->Token,"INT")==0){
             //Exp → INT
-            return_type->basic=TYPE_INT;
+            return_type->basic=T_INT;
             return return_type;
         }
         else if(strcmp(child->Token,"FLOAT")==0){
             //Exp → FLOAT
-            return_type->basic=TYPE_FLOAT;
+            return_type->basic=T_FLOAT;
             return return_type;
         }
         else if(strcmp(child->Token,"ID")==0){
             //Exp → ID
             //printf("aExp → ID\n");
-            Var find_id=find_var_table(child->Id_Type);
+            Vari find_id=find_vari_table(child->Id_Type);
             if(find_id==NULL|| find_id->is_def_struct == DEF_IN_STRUCT){
-                printf("Error type 1 at Line %d: Undefined variable \"%s\".\n",now->line,child->Id_Type);
+                printf("Error type 1 at Line %d: Undefined variiable \"%s\".\n",now->line,child->Id_Type);
                 return NULL;
             }
             return find_id->field->type;
@@ -351,7 +493,7 @@ Type Exp(P_Node now){
             Type return_type=Exp(exp);
             if(return_type==NULL)
                 return NULL;
-            if(return_type->type==BASIC&&return_type->basic==TYPE_INT)
+            if(return_type->type==BASIC&&return_type->basic==T_INT)
                 return return_type;
             
             printf("Error type 7 at Line %d: Type mismatched for operands.\n",now->line);
@@ -368,7 +510,7 @@ Type Exp(P_Node now){
         else if(strcmp(child_child->Token,"Exp")==0&&strcmp(child_child->nextbro->Token,"LB")==0);
         else if(strcmp(child_child->Token,"Exp")==0&&strcmp(child_child->nextbro->Token,"DOT")==0);
         else{
-            printf("Error type 6 at Line %d: The left-hand side of an assignment must be a variable.\n",now->line);
+            printf("Error type 6 at Line %d: The left-hand side of an assignment must be a variiable.\n",now->line);
             return NULL;
         }
         Type left_type=Exp(child);
@@ -422,7 +564,7 @@ Type Exp(P_Node now){
         }
         Type right_type=Exp(child->nextbro->nextbro);
         if(right_type==NULL) return NULL;
-        if(right_type->type==BASIC&&right_type->basic==TYPE_INT){
+        if(right_type->type==BASIC&&right_type->basic==T_INT){
             return left_type->array.elem;
         }
         else{
@@ -436,66 +578,3 @@ Type Exp(P_Node now){
         return Exp(fir_bro);
     }
 }
-// void CompSt(P_Node now,Type type){
-//     //CompSt → LC DefList StmtList RC
-//     P_Node deflist=now->firstchild->nextbro;
-//     P_Node stmtlist=deflist->nextbro;
-
-//     DefList(deflist,DEF_VAR);
-//     StmtList(stmtlist,type);
-// }
-
-// void StmtList(P_Node now,Type type){
-//     //StmtList → Stmt StmtList| empty
-//     P_Node child=now->firstchild;
-//     if(now==NULL)
-//         return;
-//     Stmt(child,type);
-//     StmtList(child->nextbro,type);
-// }
-
-// void Stmt(P_Node now,Type type){
-//     P_Node child=now->firstchild;
-//     if(strcmp(child->Token,"Exp")==0){
-//         //Stmt → Exp SEMI
-//         Exp(child);
-//     }
-//     else if(strcmp(child->Token,"CompSt")==0){
-//         //Stmt →CompSt
-//         CompSt(child,type);
-//     }
-//     // else if(strcmp(child->Token,"RETURN")==0){
-//     //     //Stmt →RETURN Exp SEMI
-//     //     Type exp_type=Exp(child->nextbro);
-//     //     if(typecmp(exp_type,type)==1){
-//     //         printf("Error type 8 at Line %d: Type mismatched for return.\n",now->line);
-//     //         return;
-//     //     }
-//     // }
-//     else if(strcmp(child->Token,"IF")==0){
-//         //Stmt →IF LP Exp RP Stmt|IF LP Exp RP Stmt ELSE Stmt
-//         P_Node exp_in_if=child->nextbro->nextbro;
-//         Type exp_type=Exp(exp_in_if);
-//         if(exp_type==NULL) return;
-//         if(exp_type->type!=BASIC||exp_type->basic!=TYPE_INT){
-//             printf("Error type 7 at Line %d: Type mismatched for operands.\n",exp_in_if->line);
-//             return;
-//         }
-//         P_Node stmt = exp_in_if->nextbro->nextbro;
-//         Stmt(stmt,type);
-//         if(!(stmt->nextbro==NULL)){
-//             Stmt(stmt->nextbro->nextbro,type);
-//         }
-//     }
-//     else if(strcmp(child->Token,"WHILE")==0){
-//         //Stmt →WHILE LP Exp RP Stmt
-//         P_Node exp_in_while=child->nextbro->nextbro;
-//         Type exp_type=Exp(exp_in_while);
-//         if(exp_type==NULL) return;
-//         if(exp_type->type!=BASIC||exp_type->basic!=TYPE_INT){
-//             printf("Error type 7 at Line %d: Type mismatched for operands.\n",exp_in_while->line);
-//             return;
-//         }
-//         Stmt(exp_in_while->nextbro->nextbro,type);
-//     }   
-// }
--- Lab2/Code/semantics.h
+++ Lab2/Code/semantics.h
@@ -7,18 +7,20 @@
 #include "node.h"
 
 #define SIZE 0x3fff
-#define TYPE_INT 0
-#define TYPE_FLOAT 1
+#define T_INT 0
+#define T_FLOAT 1
 
 #define DEF_STRUCT 0
 #define DEF_VAR 1
+#define DEF_FUNC 2
 
 #define DEF_NOT_IN_STRUCT 0
 #define DEF_IN_STRUCT 1
 
 typedef struct Type_* Type;
 typedef struct FieldList_* FieldList;
-typedef struct Var_* Var; 
+typedef struct Vari_* Vari; 
+typedef struct Function_* Function;
 struct Type_
 {
     enum {BASIC,ARRAY,STRUCT} type;
@@ -33,25 +35,41 @@ struct FieldList_
     Type type;
     FieldList next;
 };
-struct Var_{
+struct Vari_{
     int is_def_struct;
     FieldList field;
-    Var next;
+    Vari next;
     int line;
 };
+struct Function_ {
+    char* name;
+    FieldList field;
+    Type return_type;
+    int defined;
+    int line;
+    Function next;
+};
+
+
 // hash function
 unsigned int hash_pjw(char* name);
 
 // check if a name is in the table
-Var find_var_table(char* name);
+Vari find_vari_table(char* name);
 
 // add a field to the table
-void insert_var_table(FieldList field_list, int line, int in_struct);
+void insert_vari_table(FieldList field_list, int line, int in_struct);
 
 // compare two types
 int typecmp(Type a, Type b);
 int fieldcmp(FieldList a,FieldList b, int compare_name);
 
+Function find_function_table(char* name);
+// add a function to the function table
+void insert_function_table(Function func);
+
+// compare two functions
+int funccmp(Function a, Function b);
 // -----------------semantic analysis------------------------
 // ---------------------program---------------------------
 void semantic_analysis(P_Node now);
@@ -65,34 +83,35 @@ Type Specifier(P_Node now);
 // description of struct
 Type StructSpecifier(P_Node now);
 // name of struct
-char* OptTag(P_Node x);
+char* OptTag(P_Node now);
 
 // def list of variables, functions, structs (local)
-FieldList DefList(P_Node now, int restr);
+FieldList DefList(P_Node now, int judge);
 
 // def of variables, functions, structs (local)
-FieldList Def(P_Node now, int restr);
+FieldList Def(P_Node now, int judge);
 
 // name list of variables, functions, structs (local)
-FieldList DecList(P_Node now, Type type, int restr);
+FieldList DecList(P_Node now, Type type, int judge);
 
 // name of variables, functions, structs (local)
-FieldList Dec(P_Node now, Type type, int restr);
+FieldList Dec(P_Node now, Type type, int judge);
 
 // def of variables
-FieldList VarDec(P_Node now, Type type, int restr);
+FieldList VarDec(P_Node now, Type type, int judge);
 
+void FunDec(P_Node now, Type type, int judge);
 // paramlist of functions
-FieldList VarList(P_Node now, int restr);
+FieldList VarList(P_Node now, int judge);
 // param def of functions
-FieldList ParamDec(P_Node now, int restr);
+FieldList ParamDec(P_Node now, int judge);
 
-// // staments of functions
-// void CompSt(P_Node now, Type type);
-// // list of staments
-// void StmtList(P_Node now, Type type);
-// // stament
-// void Stmt(P_Node now, Type type);
+// staments of functions
+void CompSt(P_Node now, Type type);
+// list of staments
+void StmtList(P_Node now, Type type);
+// stament
+void Stmt(P_Node now, Type type);
 Type Exp(P_Node now);
 // FieldList Args(P_Node now);
 
